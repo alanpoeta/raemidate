25d2f59f20babd63470b0cd4b9dbc7a298da1fb1
alanpoeta <alanpiephi@gmail.com>
2025-08-22 17:15:05 +0200

    Removed username

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -4,7 +4,7 @@ const ProfileCard = ({ profile }) => {
   return (
     <section>
       {Object.keys(profile).map(key => {
-        if (key !== "photos" && key !== "username") {
+        if (key !== "photos") {
           return <p key={key}><b>{desnakify(key)}</b>: {profile[key]}</p>;
         }
       })}
