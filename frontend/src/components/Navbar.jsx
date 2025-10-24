975a1f019cd722b2d855ca8991d03d2ccb896f2f
alanpoeta <alanpiephi@gmail.com>
2025-10-24 09:13:05 +0200

    Fixed navbar showing logged in version when logged out

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "./helpers/authContext";
 const Navbar = () => {
   const { user, logout } = useAuth();
 
-  if (!user) return (
+  if (!user?.username) return (
       <nav>
         <Link to='/login'>Login</Link>
         <Link to='/register'>Register</Link>
@@ -13,12 +13,15 @@ const Navbar = () => {
   return (
     <nav>
       <p>{user.username}</p>
-      <Link to='/'>Home</Link>
-      <Link to='/match'>Matches</Link>
-      <Link to='/profile'>Profile</Link>
+      {user.hasProfile &&
+        <>
+          <Link to='/'>Home</Link>
+          <Link to='/match'>Matches</Link>
+          <Link to='/profile'>Profile</Link>
+        </>
+      }
       <a href="/login" onClick={e => {
         e.preventDefault();
-
         logout();
       }}>
         Logout
