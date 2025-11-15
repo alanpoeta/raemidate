a6fa221a4c739a199611b71074c4029407fc2784
alanpoeta <alanpiephi@gmail.com>
2025-11-15 14:50:14 +0100

    Fixed race condition with cleanup function being called before websocket opened, leading to two open websocket connections. Also implemented going back if websocket doesn't connect

--- frontend/src/helpers/useWebSocket.jsx
+++ frontend/src/helpers/useWebSocket.jsx
@@ -1,46 +1,47 @@
 import { useEffect, useRef, useState } from "react";
 import { auth } from "../api"
 import { useAuth } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const useWebSocket = (pathname, { onopen, onclose, onmessage, enabled = true } = {}) => {
   const { logout } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
   const socketRef = useRef(null);
+  const navigate = useNavigate();
   
   useEffect(() => {
-    const setupWebSocket = async () => {
-      if (!enabled) return;
-      
-      if (!await auth()) {
-        await logout();
-        return;
-      };
+    if (!enabled) return;
+    
+    auth()
+    .then(isAuthenticated => {
+      if (!isAuthenticated) logout();
+    })
 
-      const base = new URL(import.meta.env.VITE_API_URL);
-      base.protocol = base.protocol === 'https:' ? 'wss:' : 'ws:';
-      base.pathname = `ws/${pathname}`;
-      const accessToken = localStorage.getItem("access") || "";
-      socketRef.current = new WebSocket(base.toString(), [`Bearer.${accessToken}`]);
+    const base = new URL(import.meta.env.VITE_API_URL);
+    base.protocol = base.protocol === 'https:' ? 'wss:' : 'ws:';
+    base.pathname = `ws/${pathname}`;
+    const accessToken = localStorage.getItem("access") || "";
+    socketRef.current = new WebSocket(base.toString(), [`Bearer.${accessToken}`]);
 
-      socketRef.current.onopen = e => {
-        if (onopen) onopen(e);
-        console.log("Socket opened");
-        setIsOpen(true);
-      }
-  
-      socketRef.current.onclose = e => {
-        if (onclose) onclose(e);
-        console.error("Socket closed", e.code, e.reason);
-        setIsOpen(false);
-      };
-  
-      socketRef.current.onmessage = onmessage;
+    socketRef.current.onopen = e => {
+      if (onopen) onopen(e);
+      console.log("Socket opened");
+      setIsOpen(true);
     }
 
-    setupWebSocket();
+    socketRef.current.onclose = e => {
+      if (onclose) onclose(e);
+      console.error("Socket closed", e.code, e.reason);
+      setIsOpen(false);
+      if ([WebSocket.CLOSING, WebSocket.CLOSED].includes(socketRef.current?.readyState)) {
+        if (window.history.length > 1) navigate(-1);
+        else navigate("/", { replace: true });
+      }
+    };
 
+    socketRef.current.onmessage = onmessage;
     return () => {
-      if (socketRef.current?.readyState === WebSocket.OPEN || socketRef.current?.readyState === WebSocket.CONNECTING) {
+      if ([WebSocket.OPEN, WebSocket.CONNECTING].includes(socketRef.current?.readyState)) {
         socketRef.current?.close(1000, "disabled");
         socketRef.current = null;
       }
