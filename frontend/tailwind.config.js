e8c1ad413c72a6588c59cbafcf6694eb519521c5
alanpoeta <alanpiephi@gmail.com>
2025-12-09 00:24:35 +0100

    Moved tailwindcss import to npm instead of cdn and deleted unnecessary imports in index.html

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e11d48', // rose-600
+        secondary: '#fb7185', // rose-400
+      }
+    },
+  },
+  plugins: [],
+} 
